a6b50126ad625aa45f58ddf98bfa874bb223df9b
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2022-10-12 13:25:27 -0300

    Update Heading.stories.tsx

--- src/components/Heading.stories.tsx
+++ src/components/Heading.stories.tsx
@@ -1,28 +1,54 @@
-import {Slot} from '@radix-ui/react-slot';
-import {clsx} from 'clsx';
-import {ReactNode} from 'react';
+import {Meta,StoryObj} from '@storybook/react';
+import {Heading, HeadingProps} from './Heading';
 
+export default { //sempre esportar como default
+    title: 'Components/Heading', //titulo da documentação
+    component: Heading, //componente que estou exportando
+    args: {
+        children: 'Lorem ipsum.',
+        size: 'md',
+    },
+    argsTypes: {
+        size: {
+            options: ['sm', 'md', 'lg'],
+            control: {
+              type: 'inline-radio'
+            }
+        }
+    },
+} as Meta<HeadingProps>
 
-export interface TextProps{
-    size?: 'sm'| 'md'| 'lg';
-    children: ReactNode;
-    asChild?: boolean; 
+//para corrigir o erro no storybook.
+//precisa exportar pelo menos uma variante: que não está como default
+export const Default: StoryObj<HeadingProps> = {}
+
+export const Small: StoryObj<HeadingProps> = {
+    args: {
+        size: 'sm'
+    }
 }
 
-export function Text({size = 'md', children, asChild} : TextProps){
-    const Comp = asChild ? Slot : 'span';
-
-    return(
-       <Comp 
-         className={clsx('text-gray-100 font-sans', 
-       {
-        'text-xs': size === 'sm',
-        'text-sm': size === 'md',
-        'text-md': size === 'lg',
-       }
-    )}
-       >
-        {children}
-       </Comp>
-    );
+export const Large: StoryObj<HeadingProps> = {
+    args: {
+        size: 'lg'
+    }
 }
+
+
+export const CustomComponent: StoryObj<HeadingProps> = {
+    args:{
+        asChild: true,
+        children: (
+            <p>Testando</p>
+        )
+    },
+    argTypes: {
+      children: {
+        table: {
+            disable: true,
+        },
+      },
+    },
+};
+
+
