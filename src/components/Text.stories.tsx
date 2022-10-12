b9f1928214c4c08d8141ab1e0228c32ac516b924
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2022-10-12 11:22:42 -0300

    Create Text.stories.tsx

---
+++ src/components/Text.stories.tsx
@@ -0,0 +1,26 @@
+import {Meta,StoryObj} from '@storybook/react';
+import {Text, TextProps} from './Text';
+
+export default { //sempre esportar como default
+    title: 'Components/Text', //titulo da documentação
+    Component: Text, //componente que estou exportando
+    args: {
+        children: 'Lorem ipsum.',
+    },
+} as Meta<TextProps>
+
+//para corrigir o erro no storybook.
+//precisa exportar pelo menos uma variante: que não está como default
+export const Default: StoryObj<TextProps> = {}
+
+export const Small: StoryObj<TextProps> = {
+    args: {
+        size: 'sm'
+    }
+}
+
+export const Large: StoryObj<TextProps> = {
+    args: {
+        size: 'lg'
+    }
+}
