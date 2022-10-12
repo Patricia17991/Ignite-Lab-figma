23e3315951c0645db809c94c6724e1acc5ccdd05
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2022-10-12 14:48:04 -0300

    Update TextInput.stories.tsx

--- src/components/TextInput.stories.tsx
+++ src/components/TextInput.stories.tsx
@@ -1,15 +1,15 @@
 import {Meta,StoryObj} from '@storybook/react';
-import {TextInput, TextInputProps} from './TextInput';
+import {TextInput, TextInputInputProps} from './TextInput';
 
 export default { //sempre esportar como default
     title: 'Components/TextInput', //titulo da documentação
-    component: TextInput, //componente que estou exportando
+    component: TextInput.Input, //componente que estou exportando
     args: {
-        placeholder: 'Type your e-mail address'
+        placeholder: 'Type your e-mail address',
     },
     argsTypes: {}
-} as Meta<TextInputProps>
+} as Meta<TextInputInputProps>
 
 //para corrigir o erro no storybook.
 //precisa exportar pelo menos uma variante: que não está como default
-export const Default: StoryObj<TextInputProps> = {}
+export const Default: StoryObj<TextInputInputProps> = {}
