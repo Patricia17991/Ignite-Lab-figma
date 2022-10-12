5d0a93eba3a48b016ae7947aa361594d56184699
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2022-10-12 16:07:35 -0300

    Update TextInput.stories.tsx

--- src/components/TextInput.stories.tsx
+++ src/components/TextInput.stories.tsx
@@ -1,17 +1,34 @@
 import {Meta,StoryObj} from '@storybook/react';
-import {TextInput, TextInputInputProps, TextInputRootProps} from './TextInput';
+import {TextInput, TextInputRootProps} from './TextInput';
+import {Envelope} from 'phosphor-react';
+
 
 export default { //sempre esportar como default
     title: 'Components/TextInput', //titulo da documentação
     component: TextInput.Root, //componente que estou exportando
     args: {
-        children: (
+        children: [
+            <TextInput.Icon>
+               <Envelope/>
+            </TextInput.Icon>,
             <TextInput.Input placeholder='Type your e-mail address' />
-        ),
+        ],
      },
-    argsTypes: {}
-} as Meta<TextInputInputProps>
+    argTypes: {
+        children: {
+            table: {
+                disable: true,
+             }
+     },
+    }
+} as Meta<TextInputRootProps>
 
 //para corrigir o erro no storybook.
 //precisa exportar pelo menos uma variante: que não está como default
 export const Default: StoryObj<TextInputRootProps> = {}
+
+export const WhithoutIcon: StoryObj<TextInputRootProps> = {
+    args: {
+        children: <TextInput.Input placeholder='Type your e-mail address' />
+    }
+}
