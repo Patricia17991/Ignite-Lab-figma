8da1a23c8322cbc9a9d3295b75af489e1c4a570d
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2022-10-12 14:54:43 -0300

    Update TextInput.stories.tsx

--- src/components/TextInput.stories.tsx
+++ src/components/TextInput.stories.tsx
@@ -5,8 +5,10 @@ export default { //sempre esportar como default
     title: 'Components/TextInput', //titulo da documentação
     component: TextInput.Root, //componente que estou exportando
     args: {
-        placeholder: 'Type your e-mail address',
-    },
+        children: (
+            <TextInput.Input placeholder='Type your e-mail address' />
+        ),
+     },
     argsTypes: {}
 } as Meta<TextInputInputProps>
 
